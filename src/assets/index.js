cf77561bd4ee3c61e8369b0822cdff6c1b551a5d
Mizok <karasunohina@gmail.com>
2021-07-19 17:47:46 +0800

    update

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,31 @@
 import { Composition } from 'mkcvs'
 
-window.onload = () => {
-  let div = document.querySelector('canvas');
-  new Composition(div)
 
+function initComp(ele) {
+  const comp = new Composition(ele);
+  return comp;
+}
+
+function boot() {
+  const containerEle = document.getElementById('container');
+  const comp = initComp(containerEle);
+  // add one layer for cloth entity;
+  const clothLayer = comp.newLayer('2d');
+  const clothSimulation = new ClothSimulation(clothLayer, DEFAULT, {});
+}
+
+const DEFAULT = {}
+
+class ClothSimulation {
+  constructor(layer, defaultConfig, config) {
+    this.cvs = layer.cvs;
+    this.ctx = layer.ctx;
+    this.syncData = layer.syncData;
+  }
+}
+
+
+window.onload = () => {
+  boot();
 }
 
