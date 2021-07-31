a4907eca6da8ac07434729d4422bce2f755c40a1
Mizok <karasunohina@gmail.com>
2021-08-01 01:28:58 +0800

    update

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,21 +1,35 @@
-import { extend } from 'lodash';
-import { Composition, CtxRendering } from 'mkcvs'
-// import * as THREE from 'three';
+import "regenerator-runtime/runtime";
+import "core-js/stable";
+import { Composition } from 'mkcvs'
+import * as THREE from 'three';
 
 const DEFAULT = {}
 
 function boot() {
   const container = document.getElementById('container');
   const comp = new Composition(container);
-  const layer = comp.newLayer('');
+  const layer = comp.newLayer();
   const cloth = new ClothSimulation(layer, DEFAULT, {});
 
 }
 
 class ClothSimulation {
   constructor(layer, defaultConfig, config) {
-    // console.log(THREE);
+    this.layer = layer;
+    this.scene = new THREE.Scene();
+    this.renderer = new THREE.WebGLRenderer({ canvas: layer.cvs })
+    this.camera = new THREE.PerspectiveCamera();
+    this.init();
   }
+  init() {
+    this.setOnFrame();
+  }
+  setOnFrame() {
+    this.layer.onFrame = () => {
+      this.renderer.render(this.scene, this.camera);
+    }
+  }
+
 }
 
 
