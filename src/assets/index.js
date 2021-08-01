a38820978d61fb09e373151bcfc3be6f1cd0ac06
Mizok <karasunohina@gmail.com>
2021-08-01 15:46:07 +0800

    update

--- src/assets/index.js
+++ src/assets/index.js
@@ -18,17 +18,60 @@ class ClothSimulation {
     this.layer = layer;
     this.scene = new THREE.Scene();
     this.renderer = new THREE.WebGLRenderer({ canvas: layer.cvs })
-    this.camera = new THREE.PerspectiveCamera();
+    this.camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    this.pointLight = new THREE.PointLight(0xffffff, 0.5);
+    this.pointLight.position.set(0, 0, 100);
+    const helper = new THREE.CameraHelper(this.camera);
+    const pointLightHelper = new THREE.PointLightHelper(this.pointLight, 50, 0xffffff);
+    this.scene.add(this.ambientLight, this.pointLight, helper, pointLightHelper)
+    this.camera.position.z = 200;
     this.init();
   }
   init() {
+    this.genClothGeometry();
     this.setOnFrame();
+
   }
   setOnFrame() {
     this.layer.onFrame = () => {
+      this.refreshGeometry();
       this.renderer.render(this.scene, this.camera);
     }
   }
+  genClothGeometry() {
+    const radialware = (u, v, target) => {
+      var r = 100;
+      var y = u * 2 * r - r;
+      var z = 0;
+      var x = v * 2 * r - r;
+      target.set(x, y, z);
+    }
+    const geometry = new THREE.ParametricGeometry(radialware, 120, 120, false);
+    const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.mesh.rotation.y = Math.PI;
+    this.scene.add(this.mesh);
+  }
+
+  refreshGeometry() {
+    let period = (this.layer.syncData.frameCount % 100) / 48.5;
+
+    this.mesh.geometry.attributes.position.needsUpdate = true;
+    const positions = this.mesh.geometry.attributes.position.array;
+
+    let index = 0;
+
+    for (let i = 0; i <= positions.length; i++) {
+
+      let x = positions[index++];
+      let y = positions[index++];
+      positions[index++] = Math.sin((period + y / 200) * Math.PI) * 15;
+
+    }
+
+  }
+
 
 }
 
